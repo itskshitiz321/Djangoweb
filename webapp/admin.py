98a974b32d90d036e843c44156545b84c30b6842
skshitizraj <skshitizraj@gmail.com>
2019-10-03 15:54:36 +0545

    added school

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from .models import pointofinterests
+from .models import school
+from leaflet.admin import LeafletGeoAdmin
 
 # Register your models here.
-admin.site.register(pointofinterests)
+class schooladmin(LeafletGeoAdmin):
+    list_display=('name','Type')
+    settings_overrides =  {
+        'DEFAULT_CENTER': (28.333, 84.000),
+        'DEFAULT_ZOOM': 7,
+        'MIN_ZOOM': 5,
+        'MAX_ZOOM': 24,
+        'TILES': [('Google Terrain','http://mt0.google.com/vt/lyrs=p&hl=en&x={x}&y={y}&z={z}',''),('OSM','//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',''),('Google Satellite','http://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}','')],
+    }
+
+
+admin.site.register(school,schooladmin)
--- webapp/models.py
+++ webapp/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.gis.db import models as geomodels
 
 # Create your models here.
-class pointofinterests(models.Model):
-    name = models.CharField(max_length=30)
-    lat = models.FloatField()
-    lon = models.FloatField()
-    alt=models.FloatField()
+
+class school(models.Model):
+    name = models.CharField(max_length=40)
+    location = geomodels.PointField(srid=4326)
+    Type = models.CharField(max_length=40, blank=True)
